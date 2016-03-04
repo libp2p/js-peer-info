9a756d1786d1b09a85a5a70800784cbdfd9b9055
David Dias <daviddias.p@gmail.com>
2016-03-04 13:26:50 +0000

    new features: add and remove multiaddrs, avoid adding repeating, addSafe mode to avoid multiaddr explosion

--- src/index.js
+++ src/index.js
@@ -2,19 +2,63 @@
  * Peer represents a peer on the IPFS network
  */
 
+const Id = require('peer-id')
+
 exports = module.exports = Peer
 
-function Peer (id, multiaddrs) {
-  var self = this
-
-  if (!(self instanceof Peer)) {
-    throw new Error('Peer must be called with new')
+function Peer (peerId) {
+  if (!(this instanceof Peer)) {
+    return new Peer(peerId)
   }
 
-  if (!Array.isArray(multiaddrs)) {
-    multiaddrs = [multiaddrs]
+  if (!peerId) {
+    this.id = Id.create()
+  } else {
+    this.id = peerId
   }
 
-  self.id = id
-  self.multiaddrs = multiaddrs
+  this.multiaddrs = []
+  const observedMultiaddrs = []
+
+  this.multiaddr = {}
+
+  this.multiaddr.add = (multiaddr) => {
+    var exists = false
+    this.multiaddrs.some((m, i) => {
+      if (m.toString() === multiaddr.toString()) {
+        exists = true
+        return true
+      }
+    })
+    if (!exists) {
+      this.multiaddrs.push(multiaddr)
+    }
+  }
+
+  // to prevent multiaddr explosion
+  this.multiaddr.addSafe = (multiaddr) => {
+    var check = false
+    observedMultiaddrs.some((m, i) => {
+      if (m.toString() === multiaddr.toString()) {
+        this.multiaddr.add(multiaddr)
+        observedMultiaddrs.splice(i, 1)
+        check = true
+      }
+    })
+    if (!check) {
+      observedMultiaddrs.push(multiaddr)
+    }
+  }
+
+  this.multiaddr.rm = (multiaddr) => {
+    this.multiaddrs.some((m, i) => {
+      if (m.toString() === multiaddr.toString()) {
+        this.multiaddrs.splice(i, 1)
+        return true
+      }
+    })
+  }
+
+  // TODO: add features to fetch multiaddr using filters
+  // look at https://github.com/whyrusleeping/js-mafmt/blob/master/src/index.js
 }
